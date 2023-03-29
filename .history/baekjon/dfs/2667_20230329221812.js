600003f2ba9e1fd0ae38edc7048f2ad685fe2149
SeungYn <kwls0407@gmail.com>
2023-03-29 22:55:26 +0900

    백준 2667 내 풀이

---
+++ .history/baekjon/dfs/2667_20230329221812.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const PATH =
+  process.platform === 'linux' ? '/dev/stdin' : './baekjon/input.txt';
+const input = fs.readFileSync(PATH).toString().trim().split('\n');
+
+const n = +input[0];
+
+const visited = Array.from({ length: n }, () => new Array(n).fill(false));
+console.log(visited);
