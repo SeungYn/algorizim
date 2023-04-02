d63637a57c4799e1be20797a6ec346fffbf94103
SeungYn <kwls0407@gmail.com>
2023-04-02 22:19:08 +0900

    백준 10819 동빈나

---
+++ .history/baekjon/dfs/10819-2_20230402134728.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const PATH =
+  process.platform === 'linux' ? '/dev/stdin' : './baekjon/input.txt';
+
+const input = fs.readFileSync(PATH).toString().trim().split('\n');
+
+const n = +input[0];
+const arr = input[1].split(' ').map(Number);
+const result = [];
+const visited = new Array(n).fill(false);
+let maxValue = -Infinity;
+
+dfs(0);
+console.log(maxValue);
+
+function dfs(depth) {
+  if (depth === n) {
+    let sum = 0;
+    for (let i = 0; i < n - 1; i++) {
+      sum += Math.abs(result[i] - result[i - 1]);
+    }
+    maxValue = Math.max(sum, maxValue);
+    return;
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (visited[i]) continue;
+    visited[i] = true;
+    result.push(arr[i]);
+
+    dfs(depth + 1);
+    visited[i] = false;
+    result.pop();
+  }
+}
