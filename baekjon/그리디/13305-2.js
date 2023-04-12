9d080b8311668254c6b7e262a49b54a59c1a297f
SeungYn <kwls0407@gmail.com>
2023-04-12 18:07:47 +0900

    백준 13305

---
+++ baekjon/그리디/13305-2.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const PATH =
+  process.platform === 'linux' ? '/dev/stdin' : './baekjon/input.txt';
+
+const input = fs.readFileSync(PATH).toString().trim().split('\n');
+
+const n = +input[0];
+
+const dist = input[1].split(' ').map(Number);
+const node = input[2].split(' ').map(Number);
+
+let result = BigInt(node[0]) * BigInt(dist[0]);
+let currentIndex = 0;
+
+for (let i = 1; i < n - 1; i++) {
+  if (node[i] < node[currentIndex]) {
+    result += BigInt(node[i]) * BigInt(dist[i]);
+    currentIndex = i;
+  } else {
+    result += BigInt(node[currentIndex]) * BigInt(dist[i]);
+  }
+}
+
+console.log(String(result));
