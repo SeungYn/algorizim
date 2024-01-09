0a77f28bc04af283f9e5cd02b854602d9ca22eee
SeungYn <kwls0407@gmail.com>
2024-01-09 23:59:55 +0900

    백준: 최적화 후

--- baekjon/백트래킹/2961-3.js
+++ baekjon/백트래킹/2961-3.js
@@ -4,36 +4,31 @@ const path =
 const input = fs.readFileSync(path).toString().trim().split('\n');
 const n = +input[0];
 const ingredients = input.slice(1).map((str) => str.split(' ').map(Number));
-let visited = [];
+let visited = Array.from({ length: n }, () => false);
 let currentIngredients = [];
 let answer = Infinity;
 
-for (let i = 0; i < n; i++) {
-  visited = Array.from({ length: n }, () => false);
-  currentIngredients = [];
-  //console.log(i);
-  dfs(0, i + 1);
-}
+dfs(0, 0);
 
 console.log(answer);
 
-function dfs(dep, targetDep) {
-  if (dep >= targetDep) {
-    let sour = 1;
-    let bitterness = 0;
-
-    currentIngredients.forEach((index) => {
-      sour *= ingredients[index][0];
-      bitterness += ingredients[index][1];
-    });
-    return (answer = Math.min(answer, Math.abs(sour - bitterness)));
+function dfs(dep, start) {
+  if (dep >= 1) {
+    const [sSum, nSum] = currentIngredients.reduce(
+      (p, v) => {
+        const [s, n] = p;
+        return [s * v[0], n + v[1]];
+      },
+      [1, 0]
+    );
+    answer = Math.min(answer, Math.abs(sSum - nSum));
   }
 
-  for (let i = 0; i < n; i++) {
+  for (let i = start; i < n; i++) {
     if (visited[i]) continue;
-    currentIngredients.push(i);
+    currentIngredients.push(ingredients[i]);
     visited[i] = true;
-    dfs(dep + 1, targetDep);
+    dfs(dep + 1, i + 1);
     currentIngredients.pop();
     visited[i] = false;
   }
