61552e1323979b7912bb4fd324c62a3c7f94260f
SeungYn <kwls0407@gmail.com>
2024-02-26 16:22:59 +0900

    백준: 2512

---
+++ baekjon/binarySearch/2512-4.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const PATH =
+  process.platform === 'linux' ? '/dev/stdin' : './baekjon/input.txt';
+
+const input = fs.readFileSync(PATH).toString().trim().split('\n');
+
+const N = +input[0];
+const LIST = input[1].split(' ').map(Number);
+const MAX_SUM = +input[2];
+
+let start = 0;
+let end = MAX_SUM;
+
+if (LIST.reduce((a, b) => a + b) <= MAX_SUM)
+  return console.log(Math.max(...LIST));
+
+while (start <= end) {
+  const mid = parseInt((start + end) / 2);
+  const sumList = LIST.map((v) => (v <= mid ? v : mid)).reduce((a, b) => a + b);
+
+  if (sumList > MAX_SUM) {
+    end = mid - 1;
+  } else start = mid + 1;
+}
+
+console.log(end);
---
+++ baekjon/binarySearch/lower-upper2.js
@@ -0,0 +1,23 @@
+const arr = [1, 1, 2, 2, 2, 3, 3];
+
+function lowerBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] >= target) end = mid;
+    else start = mid + 1;
+  }
+
+  return end;
+}
+
+function upperBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] > target) end = mid;
+    else start = mid + 1;
+  }
+
+  return end;
+}
+
+console.log(lowerBound(arr, 2, 0, arr.length - 1));
