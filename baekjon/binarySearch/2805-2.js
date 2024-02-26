86f3809b8f55d7bb68d0f99b8190cd8985b1a8e6
SeungYn <kwls0407@gmail.com>
2024-02-26 19:24:06 +0900

    백준: 2805

---
+++ baekjon/binarySearch/2805-2.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const PATH =
+  process.platform === 'linux' ? '/dev/stdin' : './baekjon/input.txt';
+const input = fs.readFileSync(PATH).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const list = input[1].split(' ').map(Number);
+
+let start = 1;
+let end = Math.max(...list);
+let result = 0;
+
+while (start <= end) {
+  const mid = parseInt((start + end) / 2);
+
+  const listSum = list
+    .map((v) => (v <= mid ? 0 : v - mid))
+    .reduce((a, b) => a + b);
+
+  if (listSum >= m) {
+    start = mid + 1;
+    result = mid;
+  } else {
+    end = mid - 1;
+  }
+}
+
+console.log(result);
