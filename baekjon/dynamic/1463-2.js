2455b6d4e8756a47cc1e7872f8dbe8fd72db12a3
SeungYn <kwls0407@gmail.com>
2024-03-12 12:00:03 +0900

    백준:1463

---
+++ baekjon/dynamic/1463-2.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const PATH =
+  process.platform === 'linux' ? '/dev/stdin' : './baekjon/input.txt';
+
+const input = fs.readFileSync(PATH).toString().trim().split('\n');
+
+const n = +input[0];
+const dp = new Array(n + 1);
+dp[0] = 0;
+dp[1] = 0;
+
+for (let i = 2; i < n + 1; i++) {
+  dp[i] = dp[i - 1] + 1;
+
+  if (i % 2 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+  }
+  if (i % 3 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  }
+}
+
+console.log(dp[n]);
