2ac6585f0cedd44f63b985b960cc4b2d3ce84354
SeungYn <kwls0407@gmail.com>
2024-03-14 13:39:48 +0900

    백준: 다익스트라

--- baekjon/최단경로/dijkstra.js
+++ baekjon/최단경로/dijkstra.js
@@ -4,13 +4,13 @@ class PriorityQueue {
     this.values = [];
   }
 
-  enqueue(node) {
-    this.values.push(node);
-    this.bubbleUP();
-    return true;
+  enqueue(val) {
+    this.values.push(val);
+    this.bubbleUp();
+    return;
   }
 
-  bubbleUP() {
+  bubbleUp() {
     let currentIndex = this.values.length - 1;
     const currentNode = this.values[currentIndex];
 
@@ -25,47 +25,48 @@ class PriorityQueue {
   }
 
   dequeue() {
-    const max = this.values[0];
+    const min = this.values[0];
     const end = this.values.pop();
     if (this.values.length > 0) {
       this.values[0] = end;
       this.sinkDown();
     }
 
-    return max;
+    return min;
   }
 
   sinkDown() {
-    let currentIndex = 0;
     let currentNode = this.values[0];
+    let currentIndex = 0;
     let len = this.values.length;
 
     while (true) {
       const leftChildIndex = currentIndex * 2 + 1;
       const rightChildIndex = currentIndex * 2 + 2;
+      let leftChild, rightChild;
       let swap = null;
 
       if (leftChildIndex < len) {
-        const leftChild = this.values[leftChildIndex];
-        if (leftChild.val > currentNode.val) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.val < currentNode.val) {
           swap = leftChildIndex;
         }
       }
 
       if (rightChildIndex < len) {
-        const rightChild = this.values[rightChildIndex];
+        rightChild = this.values[rightChildIndex];
         if (
-          (swap === null && rightChild.val > currentNode.val) ||
-          (swap !== null && rightChild.val > leftChild.val)
+          (swap === null && rightChild.val < currentNode.val) ||
+          (swap !== null && leftChild.val > rightChild.val)
         ) {
           swap = rightChildIndex;
         }
       }
 
       if (!swap) break;
-
       this.values[currentIndex] = this.values[swap];
       this.values[swap] = currentNode;
+
       currentIndex = swap;
     }
   }
@@ -106,35 +107,21 @@ let graph = [
   [[4, 1]],
 ];
 
-function dijkstra(start) {
-  let pq = new PriorityQueue();
-  pq.enqueue({ num: start, val: 0 });
-  distance[start] = 0;
-  let ct = 0;
-  while (pq.values.length > 0) {
-    ct++;
-    const { num, val } = pq.dequeue();
+const q = new PriorityQueue();
+let distance = new Array(n + 1).fill(Infinity);
+q.enqueue({ num: 1, val: 0 });
+distance[1] = 0;
+let ct = 0;
+while (q.values.length > 0) {
+  ct++;
+  const { num, val } = q.dequeue();
 
-    //if (distance[num] < val) continue;
-    for (let [next, cost] of graph[num]) {
-      if (cost + val < distance[next]) {
-        distance[next] = cost + val;
-        pq.enqueue({ num: next, val: cost + val });
-      }
+  for (let [next, cost] of graph[num]) {
+    if (cost + val < distance[next]) {
+      distance[next] = cost + val;
+      q.enqueue({ num: next, val: cost + val });
     }
   }
-  console.log(ct);
 }
 
-let distance = new Array(n + 1).fill(Infinity);
-
-dijkstra(1);
 console.log(distance);
-
-const q = new PriorityQueue();
-q.enqueue({ num: 1, val: 3 });
-q.enqueue({ num: 1, val: 2 });
-q.enqueue({ num: 1, val: 1 });
-console.log(q);
-
-// ElogE 모든 간선들을 확인하며 우선순위큐는 삽입,삭제 연산 시간이 logE 이기 때문
